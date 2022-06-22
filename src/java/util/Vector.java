b2bc0c7d07533b406a229114f40b401e8d2be8e2
SDsuit <81358937+SDsuit@users.noreply.github.com>
2022-06-22 23:33:29 +0800

    Update Vector.java

--- src/java/util/Vector.java
+++ src/java/util/Vector.java
@@ -202,3 +202,108 @@ public class Vector<E> extends AbstractList<E> implements List<E>, RandomAccess,
     
     /*▲ 构造器 ████████████████████████████████████████████████████████████████████████████████┛ */
     
+    
+    
+    /*▼ 存值 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Appends the specified element to the end of this Vector.
+     *
+     * @param e element to be appended to this Vector
+     *
+     * @return {@code true} (as specified by {@link Collection#add})
+     *
+     * @since 1.2
+     */
+    // 将元素e追加到当前向量表中，返回值指示是否添加成功，添加synchronized，线程安全
+    public synchronized boolean add(E e) {
+        modCount++;
+        add(e, elementData, elementCount);
+        return true;
+    }
+    
+    /**
+     * Adds the specified component to the end of this vector,
+     * increasing its size by one. The capacity of this vector is
+     * increased if its size becomes greater than its capacity.
+     *
+     * <p>This method is identical in functionality to the
+     * {@link #add(Object) add(E)}
+     * method (which is part of the {@link List} interface).
+     *
+     * @param obj the component to be added
+     */
+    // 将元素e追加到当前向量表中
+    public synchronized void addElement(E e) {
+        modCount++;
+        add(e, elementData, elementCount);
+    }
+    
+    /**
+     * Inserts the specified element at the specified position in this Vector.
+     * Shifts the element currently at that position (if any) and any
+     * subsequent elements to the right (adds one to their indices).
+     *
+     * @param index   index at which the specified element is to be inserted
+     * @param element element to be inserted
+     *
+     * @throws ArrayIndexOutOfBoundsException if the index is out of range
+     *                                        ({@code index < 0 || index > size()})
+     * @since 1.2
+     */
+    // 将元素element添加到向量表index处
+    public void add(int index, E element) {
+        insertElementAt(element, index);
+    }
+    
+    /**
+     * Inserts the specified object as a component in this vector at the
+     * specified {@code index}. Each component in this vector with
+     * an index greater or equal to the specified {@code index} is
+     * shifted upward to have an index one greater than the value it had
+     * previously.
+     *
+     * <p>The index must be a value greater than or equal to {@code 0}
+     * and less than or equal to the current size of the vector. (If the
+     * index is equal to the current size of the vector, the new element
+     * is appended to the Vector.)
+     *
+     * <p>This method is identical in functionality to the
+     * {@link #add(int, Object) add(int, E)}
+     * method (which is part of the {@link List} interface).  Note that the
+     * {@code add} method reverses the order of the parameters, to more closely
+     * match array usage.
+     *
+     * @param obj   the component to insert
+     * @param index where to insert the new component
+     *
+     * @throws ArrayIndexOutOfBoundsException if the index is out of range
+     *                                        ({@code index < 0 || index > size()})
+     */
+    // 将元素element添加到向量表index处
+    public synchronized void insertElementAt(E element, int index) {
+        if(index>elementCount) {
+            throw new ArrayIndexOutOfBoundsException(index + " > " + elementCount);
+        }
+        
+        modCount++;
+        
+        final int s = elementCount;
+        
+        Object[] elementData = this.elementData;
+        
+        // 如果向量表已满，则需要扩容
+        if(s == elementData.length) {
+            // 对当前向量表扩容
+            elementData = grow();
+        }
+        
+        // 移动元素
+        System.arraycopy(elementData, index, elementData, index + 1, s - index);
+        
+        // 插入元素
+        elementData[index] = element;
+        
+        elementCount = s + 1;
+    }
+    
