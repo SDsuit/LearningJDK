fb2a4dfe0c2a691924d07e4029b7566afec07fe1
SDsuit <81358937+SDsuit@users.noreply.github.com>
2022-07-10 21:04:50 +0800

    Update HashSet.java

--- src/java/util/HashSet.java
+++ src/java/util/HashSet.java
@@ -194,3 +194,220 @@ public class HashSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Ser
     }
     
     /*▲ 存值 ████████████████████████████████████████████████████████████████████████████████┛ */
+
+    /*▼ 移除 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Removes the specified element from this set if it is present.
+     * More formally, removes an element {@code e} such that
+     * {@code Objects.equals(o, e)},
+     * if this set contains such an element.  Returns {@code true} if
+     * this set contained the element (or equivalently, if this set
+     * changed as a result of the call).  (This set will not contain the
+     * element once the call returns.)
+     *
+     * @param o object to be removed from this set, if present
+     *
+     * @return {@code true} if the set contained the specified element
+     */
+    // 移除指定的元素，返回值指示是否移除成功
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+    
+    /**
+     * Removes all of the elements from this set.
+     * The set will be empty after this call returns.
+     */
+    // 清空当前集合中所有元素
+    public void clear() {
+        map.clear();
+    }
+    
+    /*▲ 移除 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 包含查询 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Returns {@code true} if this set contains the specified element.
+     * More formally, returns {@code true} if and only if this set
+     * contains an element {@code e} such that
+     * {@code Objects.equals(o, e)}.
+     *
+     * @param o element whose presence in this set is to be tested
+     *
+     * @return {@code true} if this set contains the specified element
+     */
+    // 判断当前集合中是否包含元素o
+    public boolean contains(Object o) {
+        return map.containsKey(o);
+    }
+    
+    /*▲ 包含查询 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 迭代 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Returns an iterator over the elements in this set.  The elements
+     * are returned in no particular order.
+     *
+     * @return an Iterator over the elements in this set
+     *
+     * @see ConcurrentModificationException
+     */
+    // 返回当前集合的迭代器
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
+    }
+    
+    /**
+     * Creates a <em><a href="Spliterator.html#binding">late-binding</a></em>
+     * and <em>fail-fast</em> {@link Spliterator} over the elements in this
+     * set.
+     *
+     * <p>The {@code Spliterator} reports {@link Spliterator#SIZED} and
+     * {@link Spliterator#DISTINCT}.  Overriding implementations should document
+     * the reporting of additional characteristic values.
+     *
+     * @return a {@code Spliterator} over the elements in this set
+     *
+     * @since 1.8
+     */
+    // 返回描述此集合中元素的Spliterator
+    public Spliterator<E> spliterator() {
+        return new HashMap.KeySpliterator<>(map, 0, -1, 0, 0);
+    }
+    
+    /*▲ 迭代 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 杂项 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    /**
+     * Returns the number of elements in this set (its cardinality).
+     *
+     * @return the number of elements in this set (its cardinality)
+     */
+    // 返回当前集合的元素数量
+    public int size() {
+        return map.size();
+    }
+    
+    /**
+     * Returns {@code true} if this set contains no elements.
+     *
+     * @return {@code true} if this set contains no elements
+     */
+    // 判断当前集合是否为空
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+    
+    /*▲ 杂项 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /*▼ 序列化 ████████████████████████████████████████████████████████████████████████████████┓ */
+    
+    static final long serialVersionUID = -5024744406713321676L;
+    
+    /**
+     * Save the state of this {@code HashSet} instance to a stream (that is,
+     * serialize it).
+     *
+     * @serialData The capacity of the backing {@code HashMap} instance
+     * (int), and its load factor (float) are emitted, followed by
+     * the size of the set (the number of elements it contains)
+     * (int), followed by all of its elements (each an Object) in
+     * no particular order.
+     */
+    private void writeObject(ObjectOutputStream s) throws IOException {
+        // Write out any hidden serialization magic
+        s.defaultWriteObject();
+        
+        // Write out HashMap capacity and load factor
+        s.writeInt(map.capacity());
+        s.writeFloat(map.loadFactor());
+        
+        // Write out size
+        s.writeInt(map.size());
+        
+        // Write out all elements in the proper order.
+        for(E e : map.keySet())
+            s.writeObject(e);
+    }
+    
+    /**
+     * Reconstitute the {@code HashSet} instance from a stream (that is,
+     * deserialize it).
+     */
+    private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+        // Read in any hidden serialization magic
+        s.defaultReadObject();
+        
+        // Read capacity and verify non-negative.
+        int capacity = s.readInt();
+        if(capacity<0) {
+            throw new InvalidObjectException("Illegal capacity: " + capacity);
+        }
+        
+        // Read load factor and verify positive and non NaN.
+        float loadFactor = s.readFloat();
+        if(loadFactor<=0 || Float.isNaN(loadFactor)) {
+            throw new InvalidObjectException("Illegal load factor: " + loadFactor);
+        }
+        
+        // Read size and verify non-negative.
+        int size = s.readInt();
+        if(size<0) {
+            throw new InvalidObjectException("Illegal size: " + size);
+        }
+        
+        // Set the capacity according to the size and load factor ensuring that
+        // the HashMap is at least 25% full but clamping to maximum capacity.
+        capacity = (int) Math.min(size * Math.min(1 / loadFactor, 4.0f), HashMap.MAXIMUM_CAPACITY);
+        
+        // Constructing the backing map will lazily create an array when the first element is
+        // added, so check it before construction. Call HashMap.tableSizeFor to compute the
+        // actual allocation size. Check Map.Entry[].class since it's the nearest public type to
+        // what is actually created.
+        SharedSecrets.getJavaObjectInputStreamAccess().checkArray(s, Map.Entry[].class, HashMap.tableSizeFor(capacity));
+        
+        // Create backing HashMap
+        map = (this instanceof LinkedHashSet ? new LinkedHashMap<>(capacity, loadFactor) : new HashMap<>(capacity, loadFactor));
+        
+        // Read in all elements in the proper order.
+        for(int i = 0; i<size; i++) {
+            @SuppressWarnings("unchecked")
+            E e = (E) s.readObject();
+            map.put(e, PRESENT);
+        }
+    }
+    
+    /*▲ 序列化 ████████████████████████████████████████████████████████████████████████████████┛ */
+    
+    
+    
+    /**
+     * Returns a shallow copy of this {@code HashSet} instance: the elements
+     * themselves are not cloned.
+     *
+     * @return a shallow copy of this set
+     */
+    @SuppressWarnings("unchecked")
+    public Object clone() {
+        try {
+            HashSet<E> newSet = (HashSet<E>) super.clone();
+            newSet.map = (HashMap<E, Object>) map.clone();
+            return newSet;
+        } catch(CloneNotSupportedException e) {
+            throw new InternalError(e);
+        }
+    }
+    
+}
